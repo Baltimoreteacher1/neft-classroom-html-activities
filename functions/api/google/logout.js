cd1b797b1879ee71130ad44aa62f4572cd2674f8
Baltimoreteacher1 <Neftjd@gmail.com>
2026-05-25 10:40:50 -0400

    Add Google logout endpoint

---
+++ functions/api/google/logout.js
@@ -0,0 +1,14 @@
+import { clearSessionCookie, deleteSession, json } from '../../_lib/google.js';
+
+export async function onRequestPost({ request, env }) {
+  await deleteSession(request, env);
+  return json({ ok: true }, { headers: { 'set-cookie': clearSessionCookie() } });
+}
+
+export async function onRequestGet({ request, env }) {
+  await deleteSession(request, env);
+  const appBaseUrl = (env.APP_BASE_URL || '').replace(/\/$/, '') || new URL(request.url).origin;
+  return Response.redirect(`${appBaseUrl}/noam-school/?google=disconnected`, 302, {
+    headers: { 'set-cookie': clearSessionCookie() }
+  });
+}
